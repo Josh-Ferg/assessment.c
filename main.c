2a20765088793838fc9fa2436de769762cbc6630
Joshua Ferguson <josh.ferguson2014@gmail.com>
2019-04-14 13:06:25 +0000

    THE FIRST PART FINALLY WORKS!

--- main.c
+++ main.c
@@ -2,31 +2,28 @@
  
 int main()
 {
-	char message[500], letter;
-	int i, rotation;
+	unsigned char message[500], letter;
+	int i, rotarte;
 	
 	printf("Enter a message to encode:\n");
 	scanf("%[^\n]s", message);
 	printf("Enter the rotational value as an integer:\n");
-	scanf("%d", &rotation);
+	scanf("%d", &rotarte);
 	
 	for(i=0; message[i] != '\0'; ++i){
 		letter = message[i];
-		
 		if(letter >='a'&& letter <='z'){
-			letter=letter + rotation;
-			
+			letter=letter + rotarte;
 			if(letter>'z'){
-				letter= letter -'z'+'a'- 1;
+				letter= letter -26;
 			}
 			
 			message[i] = letter;
 		}
 		else if(letter >= 'A' && letter <= 'Z'){
-			letter = letter + rotation;
-			
+			letter = letter + rotarte;
 			if(letter > 'Z'){
-				letter = letter - 'Z' + 'A' - 1;
+				letter = letter - 26;
 			}
 			
 			message[i] = letter;
